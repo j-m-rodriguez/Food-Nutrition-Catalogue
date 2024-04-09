07310a6d9955d05086bf7185f43f04d6cbdf3321
unknown <jonathanmrodriguez6@gmail.com>
2024-04-08 19:36:52 -0700

    all food categories listed with pictures and able to click

--- scripts.js
+++ scripts.js
@@ -23,17 +23,15 @@
  * 
  */
 
+// importing array of food categories as "titles" and 
+// object of category and image url as "category_url"
+import links from './Data/square_images.json' assert { type: 'json' };
+import categories from './Data/categories.json' assert { type: 'json' };
+let titles = categories
+let category_url = links
+console.log(titles);
+console.log(category_url);
 
-const FRUIT_VEGETABLE_URL = "https://th.bing.com/th/id/OIP.9tLP4ORx_Idkb1V5t5BctQHaE8?w=294&h=196&c=7&r=0&o=5&dpr=1.5&pid=1.7";
-const MEAT = "https://th.bing.com/th/id/OIP.E6CyE1nxesqfY3CSA0TnzwHaEx?w=289&h=186&c=7&r=0&o=5&dpr=1.5&pid=1.7";
-const DAIRY = "https://th.bing.com/th/id/OIP.FUlusfXwD_Ekv8q_t9cVCQHaHa?w=183&h=183&c=7&r=0&o=5&dpr=1.5&pid=1.7";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fruits and Vegetables",
-    "Meat",
-    "Dairy"
-];
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
 
@@ -44,20 +42,10 @@ function showCards() {
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
     
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRUIT_VEGETABLE_URL;
-        } else if (i == 1) {
-            imageURL = MEAT;
-        } else if (i == 2) {
-            imageURL = DAIRY;
-        }
-
+    for (let title of titles) {
+        let imageURL = category_url[title]
+        
+        console.log(imageURL);
         const nextCard = templateCard.cloneNode(true); // Copy the template card
         editCardContent(nextCard, title, imageURL); // Edit title and image
         cardContainer.appendChild(nextCard); // Add new card to the     container
@@ -95,8 +83,14 @@ function removeLastCard() {
 
 // removes all cards
 function removeAllCards() {
-    for (i = titles.length; i != 0; i--) {
-        removeLastCard();
-    }
+    titles = [];
+    showCards();
 }
 
+// made the functions accessible globally because they were not working in ES6 format
+window.removeLastCard = function() {
+    removeLastCard();
+}
+window.removeAllCards = function() {
+    removeAllCards();
+}
